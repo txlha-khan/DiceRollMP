3ed8f8a61a85e0da4eef7267a1da1203433e9c95
txlha-khan <talhaaslamkhan@gmail.com>
2025-08-26 18:55:48 -0400

    intitial commit

---
+++ src/Main.java
@@ -0,0 +1,80 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
+
+    public static void main(String[] args){
+
+        //Declaring variables.
+        int timesRolled;
+
+        //Main functioning code.
+        System.out.print("Enter the number of dice to roll: ");
+        timesRolled = scanner.nextInt();
+
+        //Validate input(can't have a negative number)
+        while(timesRolled < 1){
+            System.out.print("\nNumber can't be negative, or zero, please try again: ");
+            timesRolled = scanner.nextInt();
+        }
+
+        System.out.println("Rolling " + timesRolled + " dice...");
+
+        for(int i = 0; i < timesRolled; i++){
+            int diceRoll = random.nextInt(1, 7);
+            System.out.println("\nYou rolled a " + diceRoll);
+
+            switch(diceRoll){
+                case 1 -> {
+                    System.out.println("-------");
+                    System.out.println("|     |");
+                    System.out.println("|  ●  |");
+                    System.out.println("|     |");
+                    System.out.println("-------");
+                }
+                case 2 -> {
+                    System.out.println("-------");
+                    System.out.println("|   ● |");
+                    System.out.println("|     |");
+                    System.out.println("| ●   |");
+                    System.out.println("-------");
+                }
+                case 3 -> {
+                    System.out.println("-------");
+                    System.out.println("|●    |");
+                    System.out.println("|  ●  |");
+                    System.out.println("|    ●|");
+                    System.out.println("-------");
+                }
+                case 4 -> {
+                    System.out.println("-------");
+                    System.out.println("|●   ●|");
+                    System.out.println("|     |");
+                    System.out.println("|●   ●|");
+                    System.out.println("-------");
+                }
+                case 5 -> {
+                    System.out.println("-------");
+                    System.out.println("|●   ●|");
+                    System.out.println("|  ●  |");
+                    System.out.println("|●   ●|");
+                    System.out.println("-------");
+                }
+                case 6 -> {
+                    System.out.println("-------");
+                    System.out.println("|●   ●|");
+                    System.out.println("|●   ●|");
+                    System.out.println("|●   ●|");
+                    System.out.println("-------");
+                }
+                default -> System.out.println("error");
+            }
+        }
+        
+        
+        scanner.close();
+    }
+}
